de56de338874b245dc547228941a1d8e8de5b34f
laurarantonen <laura.h.rantonen@gmail.com>
2019-12-08 16:22:58 +0200

    Add graphics and SFX

---
+++ Hexagon Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+
+    // cached reference
+    Level level;
+    GameSession gamestatus;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+    }
+}
